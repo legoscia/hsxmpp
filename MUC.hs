0ce44ded52435842267c9e2f65f8008421f7dd87
Magnus Henoch <henoch@dtek.chalmers.se>
2007-04-07 21:50:03 +0000

    Start documenting MUC

--- MUC.hs
+++ MUC.hs
@@ -1,3 +1,5 @@
+-- |Implementation of Multi-User Chat, according to XEP-0045.  This
+-- API needs more thought and will change.
 module MUC where
 
 import XMPPMonad
@@ -7,12 +9,21 @@ import JID
 
 import Data.HashTable
 
+-- |Return true if the stanza is from a JID whose \"username\@server\"
+-- part matches the given string.
 matchesBare :: String -> StanzaPredicate
 matchesBare bare = attributeMatches "from" ((==bare).getBareJid)
 
--- Join groupchat.  If successful, return function for participant
--- list.
-joinGroupchat :: String -> String -> XMPP (Maybe (IO [String]))
+-- |Join groupchat.  If successful, return function for participant
+-- list.  Currently there is no way to install an event handler for
+-- join and leave events.
+joinGroupchat :: String         -- ^Nickname to use
+              -> String         -- ^JID of room
+              -> XMPP (Maybe (IO [String])) -- ^If 'Nothing', joining
+                                            -- room failed.  If
+                                            -- 'Just', a function that
+                                            -- returns a list of room
+                                            -- participants.
 joinGroupchat nick room =
     do
       sendStanza $ XML "presence"
@@ -55,20 +66,30 @@ mucPresenceHandler participants stanza =
                        '/':n -> Just n
                        _ -> Nothing
 
+-- |Return true if the stanza is a message of type \"groupchat\".
 isGroupchatMessage :: StanzaPredicate
 isGroupchatMessage = isMessage `conj` attributeMatches "type" (=="groupchat")
 
+-- |Return true if the stanza is a private message in the named room.
 isGroupchatPrivmsg :: String -> StanzaPredicate
 isGroupchatPrivmsg room = matchesBare room `conj` attributeMatches "type" (=="chat")
                           `conj` attributeMatches "from" ((/="") . getResource)
 
-sendGroupchatMessage :: String -> String -> XMPP ()
+-- |Send a groupchat message.
+sendGroupchatMessage :: String  -- ^JID of chat room
+                     -> String  -- ^Text of message
+                     -> XMPP ()
 sendGroupchatMessage room body =
     sendStanza $ XML "message"
                    [("to",room),
                     ("type","groupchat")]
                    [XML "body" [] [CData body]]
 
+-- |Send a private message in a chat room.
+sendGroupchatPrivateMessage :: String -- ^Nick of recipient
+                            -> String -- ^JID of chat room
+                            -> String -- ^Text of message
+                            -> XMPP ()
 sendGroupchatPrivateMessage nick room body =
     sendStanza $ XML "message"
                    [("to",room++"/"++nick),
