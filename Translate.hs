de2e4c4078294a33114a6b7f873e7e985fe1e852
Magnus Henoch <henoch@dtek.chalmers.se>
2007-04-07 21:36:21 +0000

    Document Translate

--- Translate.hs
+++ Translate.hs
@@ -1,8 +1,12 @@
+-- |A very primitive translation module.  Translations are kept in
+-- files, where each pair of lines is first the \"original\" text, and
+-- then the translated text.
 module Translate 
-    (Language,
-     translate,
+    (Translatable,
+     Language,
      loadLanguage,
-     nullLanguage)
+     nullLanguage,
+     translate)
     where
 
 import System.IO
@@ -10,14 +14,20 @@ import Data.HashTable
 
 type Language = HashTable Translatable Translatable
 
+-- |This type should have more features, such as being able to insert
+-- data in the middle.
 type Translatable = String
 
+-- |Translate a string from the \"source\" language to the given
+-- language.  If the string is not present in the hashtable, return
+-- the original.
 translate :: Language -> Translatable -> IO Translatable
 translate lang from =
     do
       translated <- Data.HashTable.lookup lang from
       return $ maybe from id translated
 
+-- |Load a translation file into a hash table.
 loadLanguage :: String -> IO Language
 loadLanguage filename =
     do
@@ -38,5 +48,7 @@ loadLanguage filename =
                 addLines table rest
           addLines table [odd] = putStrLn $ "Warning: discarding " ++ odd ++ " from " ++ filename
 
+-- |Return an \"empty\" hash table, which will translate every string
+-- to itself.
 nullLanguage :: IO Language
 nullLanguage = new (==) hashString
