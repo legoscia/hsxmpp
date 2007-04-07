16a17420c51e738a035793246cee2162ea7adb4f
Magnus Henoch <henoch@dtek.chalmers.se>
2007-04-07 22:56:48 +0000

    Add introduction to documentation

--- XMPP.hs
+++ XMPP.hs
@@ -1,3 +1,43 @@
+-- |This library aims to make writing XMPP clients (in particular
+-- bots) easy and fun.  Here is a small example:
+--
+-- > import XMPP
+-- > import Network
+-- > 
+-- > -- The bot's JID is "bot@example.com"
+-- > botUsername = "bot"
+-- > botServer = "example.com"
+-- > botPassword = "secret"
+-- > 
+-- > main :: IO ()
+-- > main = withSocketsDo $
+-- >   do
+-- >     -- Connect to server...
+-- >     c <- openStream botServer
+-- >     getStreamStart c
+-- > 
+-- >     runXMPP c $ do
+-- >       -- ...authenticate...
+-- >       startAuth botUsername botServer botPassword
+-- >       sendPresence
+-- >       -- ...and do something.
+-- >       run
+-- > 
+-- > run :: XMPP ()
+-- > run = do
+-- >   -- Wait for an incoming message...
+-- >   msg <- waitForStanza (isChat `conj` hasBody)
+-- >   let sender = maybe "" id (getAttr "from" msg)
+-- >       len = length $ maybe "" id (getMessageBody msg)
+-- >   -- ...answer...
+-- >   sendMessage sender ("Your message was "++(show len)++" characters long.")
+-- >   -- ...and repeat.
+-- >   run
+--
+-- XMPP is a protocol for streaming XML also known as Jabber.  It is
+-- described in RFCs 3920 and 3921, and in a series of XMPP Extension
+-- Protocols (XEPs).  All of this can be found at
+-- <http://www.xmpp.org>.
 module XMPP ( -- * The XMPP monad
               module XMPPMonad
               -- * XML functions
